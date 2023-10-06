14c963654881a381a94fdce0e40595c1b9aafad8
nox <dark.tempus@gmail.com>
2023-10-06 15:16:09 -0500

    end

--- js/script.js
+++ js/script.js
@@ -3,6 +3,36 @@ import {
 
     quienesSomos, roma, paris, londres,imagenes 
 } from '/js/menu.js'
+
+
+
+
+
+const body = document.querySelector("body");
+
+const overlay = document.querySelector("#overlay");
+overlay.addEventListener('click', of)
+function of() {
+   overlay.style.display = "none";
+
+   body.classList.remove("hidden");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     imagenes.forEach(function(ruta) {
         let imagen = document.createElement("div");
         imagen.className = "imagen";
@@ -15,12 +45,42 @@ import {
         galeria.appendChild(imagen);
     });
 
+
+
+
+
+
+
+
+
 let enlaces = document.querySelectorAll('a');
 let tituloElemento = document.getElementById('titulo');
 let subTituloElemento = document.getElementById('subtitulo');
 let precioElemento = document.getElementById('precio');
 let parrafoElemento = document.getElementById('parrafo');
 let photoElemento = document.getElementById('photo');
+let menuButon=document.querySelector('#toogleMenu');
+let navBarMenu =document.getElementById('navMenu');
+let contenidoToogle= document.querySelector('.contenedor');
+let contacToogle=document.querySelector('#toogleContacto');
+let infoBoton=document.querySelector('.contactoToogle');
+
+menuButon.addEventListener('click',toogleMenu);
+ function toogleMenu (){
+
+   navBarMenu.classList.toggle('activeNav');
+contenidoToogle.classList.toggle('activeContendor');
+
+
+ }
+
+
+contacToogle.addEventListener('click',info);
+function info (){
+
+infoBoton.classList.toggle('contactoActive');
+
+}
 
 
 
@@ -32,9 +92,10 @@ enlaces.forEach(function (enlace) {
     
 
             enlace.classList.remove('active');
+
         });
-     
-        
+        contenidoToogle.classList.toggle('activeContendor');
+        navBarMenu.classList.toggle('activeNav');
         this.classList.add('active')
           
     
